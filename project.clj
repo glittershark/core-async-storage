686ab72b0541235278acac2c482750aedeb4cdf6
Griffin Smith <wildgriffin45@gmail.com>
2017-04-13 10:24:44 -0400

    Version 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject core-async-storage "0.2.0"
+(defproject core-async-storage "0.3.0"
   :description "Clojurescript wrapper for react-native's AsyncStorage using
                 core.async"
   :url "https://github.com/glittershark/core-async-storage"
